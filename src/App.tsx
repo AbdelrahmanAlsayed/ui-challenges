2f00461c1513b0988637e10870ff794afbc429f8
AbdelrahmanAlsayed <dev.abdelrahman7@gmail.com>
2024-10-07 21:28:16 +0300

    debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
 import AllRoutes from "./routes";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <AllRoutes />
-    </BrowserRouter>
-  );
+  return <AllRoutes />;
 }
